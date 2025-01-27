edc4c80e07665de8e8d009f9920769b54b4bb80f
Inna M <innamajkut@gmail.com>
2025-01-27 12:51:06 +0300

    first commit

--- main_test.go
+++ main_test.go
@@ -1,29 +1,58 @@
 package main
 
 import (
+	"database/sql"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	_ "modernc.org/sqlite"
 )
 
 func Test_SelectClient_WhenOk(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	require.NoError(t, err)
+	defer db.Close()
 
 	clientID := 1
 
 	// напиши тест здесь
+	got, err := selectClient(db, clientID)
+
+	require.NoError(t, err)
+	require.Equal(t, clientID, got.ID)
+
+	assert.NotEmpty(t, got.Birthday)
+	assert.NotEmpty(t, got.Email)
+	assert.NotEmpty(t, got.FIO)
+	assert.NotEmpty(t, got.Login)
 }
 
 func Test_SelectClient_WhenNoClient(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	require.NoError(t, err)
+	defer db.Close()
 
 	clientID := -1
 
 	// напиши тест здесь
+	got, err := selectClient(db, clientID)
+	require.Equal(t, sql.ErrNoRows, err)
+
+	assert.Empty(t, got.Birthday)
+	assert.Empty(t, got.Email)
+	assert.Empty(t, got.FIO)
+	assert.Empty(t, got.ID)
+	assert.Empty(t, got.Login)
 }
 
 func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	require.NoError(t, err)
+	defer db.Close()
 
 	cl := Client{
 		FIO:      "Test",
@@ -33,10 +62,21 @@ func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
 	}
 
 	// напиши тест здесь
+	id, err := insertClient(db, cl)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	got, err := selectClient(db, id)
+	require.NoError(t, err)
+	require.Equal(t, cl, got)
 }
 
 func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+	require.NoError(t, err)
+	defer db.Close()
 
 	cl := Client{
 		FIO:      "Test",
@@ -46,4 +86,20 @@ func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
 	}
 
 	// напиши тест здесь
+	id, err := insertClient(db, cl)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	got, err := selectClient(db, cl.ID)
+	require.NoError(t, err)
+	require.Equal(t, cl, got)
+
+	err = deleteClient(db, cl.ID)
+	require.NoError(t, err)
+
+	got1, err := selectClient(db, cl.ID)
+
+	require.Equal(t, sql.ErrNoRows, err)
+	require.Empty(t, got1)
 }
